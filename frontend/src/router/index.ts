a676ef1e7e74ff98f83fa6edaccb948f87595691
JuanmaZapataOlaya <124525725+JuanmaZapataOlaya@users.noreply.github.com>
2026-02-17 22:40:56 +0000

    Tutorial4

--- frontend/src/router/index.ts
+++ frontend/src/router/index.ts
@@ -1,11 +1,10 @@
 import { createRouter, createWebHistory } from 'vue-router';
-
 import HomeView from '@/views/HomeView.vue';
-
 import AboutView from '@/views/AboutView.vue';
-
 import ContactView from '@/views/ContactView.vue';
-
+import BooksIndexView from '@/views/BooksIndexView.vue'; 
+import BooksShowView from '@/views/BooksShowView.vue'; 
+import BooksCreateView from '@/views/BooksCreateView.vue';
 
 const router = createRouter({
 
@@ -18,6 +17,12 @@ routes: [
 { path: '/about', name: 'about', component: AboutView, meta: { title: 'About' } },
 
 {path : '/contact', name : 'contact', component : ContactView, meta: { title: 'Contact' }},
+
+{ path: '/books', name: 'books', component: BooksIndexView, meta: { title: 'Books' } }, 
+
+{ path: '/books/create', name: 'books.create', component: BooksCreateView, meta: { title: 'Create Book' } },
+
+{ path: '/books/:id', name: 'book', component: BooksShowView, meta: { title: 'Book' } }, 
 
 ],
 
