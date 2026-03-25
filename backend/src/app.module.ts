fe351bce0e5ef04fc90b5ee9bb8a9b8c3713f328
JuanManuel <jmzapatao@eafit.edu.co>
2026-03-24 22:22:04 -0500

    Tutorial6_Completo

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,10 +1,16 @@
-import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { Module } from '@nestjs/common'; 
+import { HomeModule } from './home/home.module'; 
+import { TypeOrmModule } from '@nestjs/typeorm'; 
+import { BooksModule } from './books/books.module'; 
 
-@Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
-})
+@Module({ 
+imports: [    TypeOrmModule.forRoot({ 
+      type: 'sqlite', 
+      database: 'database.sqlite', 
+      autoLoadEntities: true, 
+      synchronize: true, 
+    }), HomeModule,
+      BooksModule,
+    ], 
+}) 
 export class AppModule {}
